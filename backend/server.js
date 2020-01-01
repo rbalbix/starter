e70e7fe3fdd2351d66c992c2deba79cbb801e90b
Ricardo Balbi <rbalbi@gmail.com>
2020-01-01 21:19:56 +0000

    update de

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+// iniciando o app
 const app = express();
 
+// iniciando o bd
+mongoose.connect("mongodb+srv://starter:starter@cluster0-kj5ow.mongodb.net/starter?retryWrites=true&w=majority")
+
+//rotas
 app.get("/", (req, res) => {
     res.send("Hello starter !");
 });
