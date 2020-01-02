6e5b6a7730424069ddb758504b40cb62273b1b2c
Ricardo Balbi <rbalbi@gmail.com>
2020-01-02 20:59:34 -0300

    update paginate. Added cors.

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
 
 // iniciando o app
 const app = express();
 
+
+app.use(cors());
+
 // iniciando o bd
 mongoose.connect("mongodb+srv://starter:starter@cluster0-kj5ow.mongodb.net/starter?retryWrites=true&w=majority"
-    , {useNewUrlParser : true
-        , useUnifiedTopology : true
+    , {
+        useNewUrlParser: true
+        , useUnifiedTopology: true
     });
 
 requireDir("./src/models");
