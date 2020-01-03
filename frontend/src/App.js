c7fd1f67aa9e289f11061c24cede6caac363bffd
Ricardo Balbi de Faria Junior <RJUNIOR@int.eletrobras.gov.br>
2020-01-03 17:00:59 -0300

    Searching products from api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import Header from './components/Header'
-
 import './styles.css'
+
+import Header from './components/Header'
+import Main from './pages/main'
 
 const App = () => (
     <div className="App">
       <Header/>
+      <Main/>
     </div>
 )
 
